5a00389afaf8f3829405e464541ab9b71ee88390
dannydarku <dannydarku@gmail.com>
2019-05-09 23:06:07 -0400

    complete

---
+++ app/assets/javascripts/components/_new_item.js.jsx
@@ -0,0 +1,13 @@
+const NewFruit = (props) => {
+  
+    let formFields = {}
+ 
+    return(
+      <form onSubmit={ (e) => {e.preventDefault();props.handleFormSubmit(formFields.name.value, formFields.description.value); e.target.reset();}
+}>
+        <input ref={input => formFields.name = input} placeholder='Enter the name of the fruit'/>
+        <input ref={input => formFields.description = input} placeholder='Enter a description' />
+        <button>Submit</button>
+      </form>
+    )
+}
